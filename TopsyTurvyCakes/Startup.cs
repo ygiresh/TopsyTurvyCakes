759b53a06f341b4fe97b057c579179219682b431
Gireshbabu Yemparala <A1830486@mail.niu.edu>
2019-07-26 16:05:02 -0500

    Implementing DI with RecipeService

--- TopsyTurvyCakes/Startup.cs
+++ TopsyTurvyCakes/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TopsyTurvyCakes.Models;
 
 namespace TopsyTurvyCakes
 {
@@ -15,7 +16,9 @@ namespace TopsyTurvyCakes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
+            services.AddScoped<IRecipesService, RecipesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
